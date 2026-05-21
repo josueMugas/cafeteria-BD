1d3a2bf47b98b5acd56f6873d8a398b3c8cecdf1
Matias-Adan <matias.adanet32@gmail.com>
2026-05-20 22:46:26 -0300

    Update SQL STORE PROCEDURES.sql

--- SQL STORE PROCEDURES.sql
+++ SQL STORE PROCEDURES.sql
@@ -14,45 +14,60 @@ END //
 DELIMITER //
 */
     
+
+
 -- 2. Recibe el ID de un producto y el nuevo precio. Lo actualiza.
-CREATE PROCEDURE ActualizarPrecio(IN p_id_prod INT, IN p_nuevo_precio DECIMAL(10,2))
+DELIMITER //
+CREATE PROCEDURE ActualizarPrecio(
+    IN p_id_prod INT, 
+    IN p_nuevo_precio DECIMAL(10,2)
+)
 BEGIN
-    UPDATE productos SET precio = p_nuevo_precio
+    UPDATE productos 
+    SET precio = p_nuevo_precio
     WHERE id_producto = p_id_prod;
 END //
-
 DELIMITER //
 
--- 3. Recibe el ID de un usuario y muestra su historial de pedidos.
-CREATE PROCEDURE ConsultarHistorialCliente(IN p_id_usuario INT)
+-- 3. Recibe el ID de un usuario y muestra su historial de pedidos con los nombres de los productos.
+DELIMITER //
+CREATE PROCEDURE ConsultarHistorialCliente(
+    IN p_id_usuario INT
+)
 BEGIN
-    SELECT * FROM pedidos
-    WHERE id_usuario = p_id_usuario;
+    SELECT p.id_pedido, p.fecha, pr.nombre AS producto, p.cantidad_producto, pr.precio
+    FROM pedidos p
+    INNER JOIN productos pr ON p.id_producto = pr.id_producto
+    WHERE p.id_usuario = p_id_usuario;
 END //
-
 DELIMITER //
 
 -- 4. Recibe el ID de un producto y devuelve su nombre y precio.
-CREATE PROCEDURE ObtenerInfoProducto(IN p_id_producto INT)
+DELIMITER //
+CREATE PROCEDURE ObtenerInfoProducto(
+    IN p_id_producto INT
+)
 BEGIN
-    SELECT nombre, precio FROM productos
+    SELECT nombre, precio 
+    FROM productos
     WHERE id_producto = p_id_producto;
 END //
-
 DELIMITER //
 
--- 5- Recibe los datos de una persona y los guarda en la tabla clientes.
+-- 5. Recibe los datos de una persona y los guarda en la tabla usuarios y clientes
 DELIMITER //
-CREATE PROCEDURE InsertarCliente(c_nom VARCHAR(50), c_ape VARCHAR(50), c_mail VARCHAR(50), c_contra VARCHAR(50), c_dom VARCHAR(50))
+CREATE PROCEDURE InsertarCliente(
+    IN c_nom VARCHAR(50), 
+    IN c_mail VARCHAR(50), 
+    IN c_contra VARCHAR(255), 
+    IN c_dom VARCHAR(50)
+)
 BEGIN
-    INSERT INTO usuarios (nombre, apellido, mail, contraseña, domicilio) 
-    VALUES (c_nom, c_ape, c_mail, c_contra, c_dom);
+    
+    INSERT INTO usuarios (nombre, mail, contraseña, domicilio, Rol) 
+    VALUES (c_nom, c_mail, c_contra, c_dom, 'Cliente');
 
-    INSERT INTO clientes (id_usuario) 
-    VALUES ((SELECT id_usuario FROM usuarios WHERE mail = c_mail));
-
-    INSERT INTO cartera (id_usuario, saldo) 
+    INSERT INTO clientes (id_usuario, fondos) 
     VALUES ((SELECT id_usuario FROM usuarios WHERE mail = c_mail), 0.00);
 END //
 DELIMITER //
-
